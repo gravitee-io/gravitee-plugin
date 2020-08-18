1aec5ad42e9f0346078d289b9d4299dc81ee6deb
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2020-08-18 10:15:28 +0200

    feat(cockpit): cockpit plugin
    Closes gravitee-io/issues#4113

---
+++ gravitee-plugin-cockpit/src/main/java/io/gravitee/plugin/cockpit/CockpitPluginHandler.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.plugin.cockpit;
+
+import io.gravitee.cockpit.api.CockpitConnector;
+import io.gravitee.plugin.core.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class CockpitPluginHandler extends AbstractSpringPluginHandler<CockpitConnector> {
+
+    @Autowired
+    private PluginClassLoaderFactory<Plugin> pluginClassLoaderFactory;
+
+    @Override
+    public boolean canHandle(Plugin plugin) {
+        return plugin.type() == PluginType.COCKPIT;
+    }
+
+    @Override
+    protected String type() {
+        return  PluginType.COCKPIT.name();
+    }
+
+    @Override
+    protected void register(CockpitConnector connector) {
+
+        try {
+            // Start the cockpit connector.
+            connector.start();
+        } catch (Exception e) {
+            logger.error("Unexpected error while starting cockpit controller", e);
+        }
+    }
+
+    @Override
+    protected ClassLoader getClassLoader(Plugin plugin) throws Exception {
+        return pluginClassLoaderFactory.getOrCreateClassLoader(plugin, this.getClass().getClassLoader());
+    }
+}
